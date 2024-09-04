2ea3b5774d6336aa192f245ad36d3f4321f1c63c
abhishek <abhishek619raj@gmail.com>
2024-09-04 10:39:36 +0530

    Handle new lines as delimiter

--- string_calculator.py
+++ string_calculator.py
@@ -1,8 +1,10 @@
 def add(numbers: str) -> int:
     if not numbers:
         return 0
+    numbers = numbers.replace('\n', ',')
     nums = map(int, numbers.split(','))
     return sum(nums)
+
 
 
 
@@ -14,3 +16,6 @@ def test_two_numbers_comma_separated():
 
 def test_multiple_numbers_comma_separated():
     assert add("1,2,3,4") == 10
+
+def test_newline_as_delimiter():
+    assert add("1\n2,3") == 6
