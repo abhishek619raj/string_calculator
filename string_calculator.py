d986c7166905289ff33e4c259bfb561f93502323
abhishek <abhishek619raj@gmail.com>
2024-09-04 10:39:53 +0530

    Support custom delimiters

--- string_calculator.py
+++ string_calculator.py
@@ -1,9 +1,15 @@
+import re
+
 def add(numbers: str) -> int:
     if not numbers:
         return 0
+    if numbers.startswith("//"):
+        delimiter, numbers = re.match("//(.)\n(.*)", numbers).groups()
+        numbers = numbers.replace(delimiter, ',')
     numbers = numbers.replace('\n', ',')
     nums = map(int, numbers.split(','))
     return sum(nums)
+
 
 
 
@@ -19,3 +25,6 @@ def test_multiple_numbers_comma_separated():
 
 def test_newline_as_delimiter():
     assert add("1\n2,3") == 6
+
+def test_custom_delimiter():
+    assert add("//;\n1;2") == 3
