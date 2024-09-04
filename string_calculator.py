b188511dd879166416db6a659e817fa6668b4efa
abhishek <abhishek619raj@gmail.com>
2024-09-04 10:38:41 +0530

    Handle two numbers separated by a comma

--- string_calculator.py
+++ string_calculator.py
@@ -1,8 +1,13 @@
 def add(numbers: str) -> int:
     if not numbers:
         return 0
-    return int(numbers)
+    nums = map(int, numbers.split(','))
+    return sum(nums)
+
 
 
 def test_single_number_returns_value():
     assert add("1") == 1
+
+def test_two_numbers_comma_separated():
+    assert add("1,2") == 3
